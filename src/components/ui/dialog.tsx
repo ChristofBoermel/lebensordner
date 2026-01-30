23b5f8452becdb49839579e459a34ec85aaf4aa6
Chris <araea2@protonmail.com>
2026-01-31 00:05:00 +0100

    fixes

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -37,11 +37,10 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed top-[50%] z-50 grid w-full max-w-lg translate-y-[-50%] gap-4 border border-warmgray-200 bg-white p-6 shadow-lg duration-200",
+        "fixed left-[50%] top-[50%] z-50 grid w-[calc(100%-2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-warmgray-200 bg-white p-6 shadow-lg duration-200 sm:w-full",
         "max-h-[85vh] overflow-y-auto",
         "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
-        "rounded-lg sm:rounded-lg",
-        "left-4 right-4 sm:left-[50%] sm:right-auto sm:translate-x-[-50%]",
+        "rounded-xl",
         className
       )}
       {...props}
@@ -62,7 +61,8 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
+      "flex flex-col space-y-1.5 text-center sm:text-left pr-8 sm:pr-0",
+      "senior-mode:pr-12 sm:senior-mode:pr-0",
       className
     )}
     {...props}
